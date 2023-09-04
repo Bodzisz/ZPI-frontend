551048e74f80a7d80fbe1ad14b6efe87d44ecc22
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-09-05 00:13:01 +0200

    Add mantine ui and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import {
+  MantineProvider,
+  Title,
+  TitleProps,
+  createPolymorphicComponent,
+} from "@mantine/core";
+import styled from "@emotion/styled";
 
-function App() {
-  const [count, setCount] = useState(0);
+const _CenterTitle = styled(Title)`
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+`;
 
+const CenterTitle = createPolymorphicComponent<"title", TitleProps>(
+  _CenterTitle
+);
+
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <CenterTitle>Hala Madrid!</CenterTitle>
+    </MantineProvider>
   );
 }
-
-export default App;
