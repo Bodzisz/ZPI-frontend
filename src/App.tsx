50500efebbbb71ba76e7c386e624bf593b89d2ab
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-10-13 18:52:52 +0200

    Create navigation menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import {
   Title,
   TitleProps,
   createPolymorphicComponent,
+  createTheme,
 } from "@mantine/core";
 import styled from "@emotion/styled";
-import { useEffect } from "react";
-import { checkResponseStatus, getApiUrl } from "./components/api/apiConfig";
+import { useEffect, useState } from "react";
+import { checkResponseStatus, getApiUrl } from "./api/apiConfig";
+import AppHeader from "./components/AppHeader/AppHeader";
+import "@mantine/core/styles.css";
+
+const theme = createTheme({
+  primaryColor: "cyan",
+});
 
 const _CenterTitle = styled(Title)`
   display: flex;
@@ -19,9 +26,25 @@ const CenterTitle = createPolymorphicComponent<"title", TitleProps>(
 );
 
 export default function App() {
+  const [selectedTab, setSelectedTab] = useState(1);
+
   useEffect(() => {
     logApiHealth();
+    console.log(selectedTab);
   }, []);
+
+  const getContent = () => {
+    switch (selectedTab) {
+      case 1:
+        return <CenterTitle>Strona główna</CenterTitle>;
+      case 2:
+        return <CenterTitle>Atrakcje</CenterTitle>;
+      case 3:
+        return <CenterTitle>Kontakt</CenterTitle>;
+      default:
+        return <></>;
+    }
+  };
 
   const logApiHealth = (): void => {
     const apiUrl = getApiUrl();
@@ -39,8 +62,9 @@ export default function App() {
   };
 
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <CenterTitle>Hala Madrid!</CenterTitle>
+    <MantineProvider theme={theme} defaultColorScheme="auto">
+      <AppHeader selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      <div>{getContent()}</div>
     </MantineProvider>
   );
 }
