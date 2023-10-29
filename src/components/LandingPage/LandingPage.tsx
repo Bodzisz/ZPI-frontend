df52b80d96d447c3e0b9ac03341e56619f0b0072
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-10-29 23:36:08 +0100

    Add landing page and logo

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Container, Paper, Text } from "@mantine/core";
+import { Center, Container, Group, Text } from "@mantine/core";
 import {
   CitySearchInput,
   AttractionSearchInput,
 } from "./SearchInputs/SearchInputs";
+import classes from "./LandingPage.module.css";
 
 const LandingPage = () => {
   const [cityQuery, setCityQuery] = useState("");
@@ -19,47 +20,47 @@ const LandingPage = () => {
 
   return (
     <>
-      <Text
-        size="s"
-        style={{
-          paddingLeft: "300px",
-          paddingRight: "300px",
-          paddingBottom: "50px",
-          paddingTop: "20px",
-        }}
-      >
-        Witaj na stronie internetowej poświęconej atrakcjom Dolnego Śląska!
-        Odkryj piękno naszego regionu, pełnego historycznych zabytków,
-        malowniczych krajobrazów i niezwykłych atrakcji. Dolny Śląsk zachwyca
-        swoją różnorodnością, od urokliwych miast i wsi po przyrodnicze cuda.
-      </Text>
+      <div className={classes.headerPic}>
+        <div className={classes.content}>
+          <h1 className={classes.title}>
+            <Center>
+              <Text
+                pl={"7px"}
+                component="span"
+                inherit
+                variant="gradient"
+                gradient={{ from: "cyan", to: "white" }}
+              >
+                Znajdź z nami atrakcje!
+              </Text>
+            </Center>
+          </h1>
 
-      <div style={{ display: "flex", justifyContent: "center", gap: "200px" }}>
-        <Paper>
-          <Container size="lg">
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                gap: "200px",
-                paddingBottom: 60,
-              }}
-            >
-              <CitySearchInput
-                value={cityQuery}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-                  handleCitySearch(e.target.value)
-                }
-              />
-              <AttractionSearchInput
-                value={attractionQuery}
-                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-                  handleAttractionSearch(e.target.value)
-                }
-              />
-            </div>
+          <Container size={"md"} pt={"50px"} pb={"100px"}>
+            <Text size="m" c={"white"}>
+              Witaj na stronie internetowej poświęconej atrakcjom Dolnego
+              Śląska! Odkryj piękno naszego regionu, pełnego historycznych
+              zabytków, malowniczych krajobrazów i niezwykłych atrakcji. Dolny
+              Śląsk zachwyca swoją różnorodnością, od urokliwych miast i wsi po
+              przyrodnicze cuda.
+            </Text>
           </Container>
-        </Paper>
+
+          <Group justify="center" gap={"200px"} visibleFrom="sm">
+            <CitySearchInput
+              value={cityQuery}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                handleCitySearch(e.target.value)
+              }
+            />
+            <AttractionSearchInput
+              value={attractionQuery}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+                handleAttractionSearch(e.target.value)
+              }
+            />
+          </Group>
+        </div>
       </div>
     </>
   );
--- src/components/LandingPage/SearchInputs/SearchInputs.tsx
+++ src/components/LandingPage/SearchInputs/SearchInputs.tsx
@@ -1,37 +1,38 @@
-import React, { ChangeEventHandler } from 'react';
-import { TextInput, Paper, Button } from '@mantine/core';
+import React, { ChangeEventHandler } from "react";
+import { TextInput, Button } from "@mantine/core";
 
 interface CitySearchInputProps {
   value: string;
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
-const CitySearchInput: React.FC<CitySearchInputProps> = ({ value, onChange}) => {
-
+const CitySearchInput: React.FC<CitySearchInputProps> = ({
+  value,
+  onChange,
+}) => {
   const handleSearch = () => {
     //search logic
   };
   return (
-    <Paper >
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-      <TextInput
-        label="Miasta"
-        placeholder="Wyszukaj miasto"
-        value={value}
-        onChange={onChange}
-        style={{ width: '280px' }} 
-        
-      />
-      <Button
+    <div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <TextInput
+          label="Miasta"
+          placeholder="Wyszukaj miasto"
+          value={value}
+          onChange={onChange}
+          c={"white"}
+        />
+        <Button
           variant="filled"
-          color="teal"
           size="sm"
-          style={{ marginLeft: '8px', marginTop: '25px' }}
+          style={{ marginLeft: "8px", marginTop: "25px" }}
           onClick={handleSearch}
-        >Search</Button>
+        >
+          Szukaj
+        </Button>
+      </div>
     </div>
-    </Paper>
-    
   );
 };
 
@@ -40,30 +41,33 @@ interface AttractionSearchInputProps {
   onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
-const AttractionSearchInput: React.FC<AttractionSearchInputProps> = ({ value, onChange }) => {
-
+const AttractionSearchInput: React.FC<AttractionSearchInputProps> = ({
+  value,
+  onChange,
+}) => {
   const handleSearch = () => {
     //search logic
   };
   return (
-    <Paper>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-      <TextInput
-        label="Atrakcje"
-        placeholder="Wyszukaj atrakcje"
-        value={value}
-        onChange={onChange}
-        style={{ width: '280px' }} 
-      />
-    <Button
+    <div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <TextInput
+          label="Atrakcje"
+          placeholder="Wyszukaj atrakcje"
+          value={value}
+          onChange={onChange}
+          c={"white"}
+        />
+        <Button
           variant="filled"
-          color="teal"
           size="sm"
-          style={{ marginLeft: '8px', marginTop: '25px' }} 
+          style={{ marginLeft: "8px", marginTop: "25px" }}
           onClick={handleSearch}
-          >Search</Button>
-        </div>
-    </Paper>
+        >
+          Szukaj
+        </Button>
+      </div>
+    </div>
   );
 };
 
