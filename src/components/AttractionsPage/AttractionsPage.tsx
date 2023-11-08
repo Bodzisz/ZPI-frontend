0702690d5402943ce8feb824a5d28e47aa381dad
Kacper Wójcicki <kacper.wojcicki@protonmail.com>
2023-11-08 19:00:45 +0100

    Add pagination to attraction list

--- src/components/AttractionsPage/AttractionsPage.tsx
+++ src/components/AttractionsPage/AttractionsPage.tsx
@@ -1,20 +1,41 @@
 import { useEffect, useState } from "react";
 import { AttractionCard } from "../AttractionCard/AttractionCard";
-import { fetchAtrractions } from "../../api/apiFetchRequests";
+import { fetchAtrractionsByPage } from "../../api/apiFetchRequests";
 import { FetchError } from "../../api/interfaces/FetchError";
-import { Attraction } from "../../api/interfaces/Attraction";
-import { SimpleGrid, Loader, Center, Container } from "@mantine/core";
+import { AttractionList } from "../../api/interfaces/Attraction";
+import {
+  SimpleGrid,
+  Loader,
+  Center,
+  Container,
+  Pagination,
+} from "@mantine/core";
+import { useWindowScroll } from "@mantine/hooks";
 import ServerError from "../ServerError/ServerError";
+
+const starterAttractionList: AttractionList = {
+  content: [],
+  totalPages: 0,
+  totalElements: 0,
+  number: 0,
+  size: 0,
+  numberOfElements: 0,
+};
 
 const AttractionsPage = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [errorStatus, setErorrStatus] = useState<number | null>(null);
-  const [attractions, setAttractions] = useState<Attraction[]>([]);
+  const [attractions, setAttractions] = useState<AttractionList>(
+    starterAttractionList
+  );
+  const [activePage, setActivePage] = useState<number>(0);
+  const [, scrollTo] = useWindowScroll();
 
   useEffect(() => {
-    fetchAtrractions()
+    setIsLoading(true);
+    fetchAtrractionsByPage(activePage)
       .then((data) => {
-        setAttractions(data.content);
+        setAttractions(data);
       })
       .catch((error: FetchError) => {
         setErorrStatus(error.status);
@@ -22,21 +43,41 @@ const AttractionsPage = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, [activePage]);
+
+  const handlePageChange = (page: number) => {
+    setActivePage(page - 1);
+    scrollTo({ y: 0 });
+  };
 
   const getAttractionsList = () => {
     return (
-      <Center>
-        <SimpleGrid
-          cols={{ base: 1, sm: 2, lg: 4 }}
-          spacing={{ base: 10, sm: "xl" }}
-          verticalSpacing={{ base: "md", sm: "xl" }}
-        >
-          {attractions.map((attraction) => {
-            return <AttractionCard attraction={attraction}></AttractionCard>;
-          })}
-        </SimpleGrid>
-      </Center>
+      <div style={{ paddingBottom: "100px" }}>
+        <Center>
+          <SimpleGrid
+            cols={{ base: 1, sm: 2, lg: 4 }}
+            spacing={{ base: 10, sm: "xl" }}
+            verticalSpacing={{ base: "md", sm: "xl" }}
+          >
+            {attractions.content.map((attraction) => {
+              return (
+                <AttractionCard
+                  attraction={attraction}
+                  key={attraction.id}
+                ></AttractionCard>
+              );
+            })}
+          </SimpleGrid>
+        </Center>
+        <Center pt={"40px"} pb={"20px"}>
+          <Pagination
+            total={attractions.totalPages}
+            value={activePage + 1}
+            onChange={handlePageChange}
+            withEdges
+          />
+        </Center>
+      </div>
     );
   };
 
